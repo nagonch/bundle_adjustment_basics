6ce30a306437aad96780901ae775422785694120
N G <kolchindru@gmail.com>
2025-03-13 16:01:59 +1100

    fix

--- bundle_adjustment.py
+++ bundle_adjustment.py
@@ -5,7 +5,45 @@ import os
 import numpy as np
 
 
+def read_bal_data(file_name):
+    with bz2.open(file_name, "rt") as file:
+        n_cameras, n_points, n_observations = map(int, file.readline().split())
+
+        camera_indices = np.empty(n_observations, dtype=int)
+        point_indices = np.empty(n_observations, dtype=int)
+        points_2d = np.empty((n_observations, 2))
+
+        for i in range(n_observations):
+            camera_index, point_index, x, y = file.readline().split()
+            camera_indices[i] = int(camera_index)
+            point_indices[i] = int(point_index)
+            points_2d[i] = [float(x), float(y)]
+
+        camera_params = np.empty(n_cameras * 9)
+        for i in range(n_cameras * 9):
+            camera_params[i] = float(file.readline())
+        camera_params = camera_params.reshape((n_cameras, -1))
+
+        points_3d = np.empty(n_points * 3)
+        for i in range(n_points * 3):
+            points_3d[i] = float(file.readline())
+        points_3d = points_3d.reshape((n_points, -1))
+    return camera_params, points_3d, camera_indices, point_indices, points_2d
+
+
 if __name__ == "__main__":
     dataset_url = "http://grail.cs.washington.edu/projects/bal/data/ladybug/problem-49-7776-pre.txt.bz2"
     filename = "dataset.txt.bz2"
-    urllib.request.urlretrieve(dataset_url, filename)
+    if not os.path.isfile(filename):
+        urllib.request.urlretrieve(dataset_url, filename)
+
+    camera_params, points_3d, camera_indices, point_indices, points_2d = read_bal_data(
+        filename
+    )
+    print(
+        camera_params.shape,
+        points_3d.shape,
+        camera_indices.shape,
+        point_indices.shape,
+        points_2d.shape,
+    )
