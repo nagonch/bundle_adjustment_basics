3273affa54fa67c9a167881c999635b682911bc8
N G <kolchindru@gmail.com>
2025-03-13 16:00:36 +1100

    fix

--- bundle_adjustment.py
+++ bundle_adjustment.py
@@ -1,9 +1,11 @@
-import urllib
+from __future__ import print_function
+import urllib.request
 import bz2
 import os
 import numpy as np
-from __future__ import print_function
 
 
 if __name__ == "__main__":
     dataset_url = "http://grail.cs.washington.edu/projects/bal/data/ladybug/problem-49-7776-pre.txt.bz2"
+    filename = "dataset.txt.bz2"
+    urllib.request.urlretrieve(dataset_url, filename)
