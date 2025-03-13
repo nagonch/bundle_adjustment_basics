93b063adfcc5eae9c34c7ada28c5b50f1a6a6e94
N G <kolchindru@gmail.com>
2025-03-13 15:58:27 +1100

    fix

---
+++ bundle_adjustment.py
@@ -0,0 +1,9 @@
+import urllib
+import bz2
+import os
+import numpy as np
+from __future__ import print_function
+
+
+if __name__ == "__main__":
+    dataset_url = "http://grail.cs.washington.edu/projects/bal/data/ladybug/problem-49-7776-pre.txt.bz2"
