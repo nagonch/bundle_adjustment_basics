8ce24ade95b237f8222fd288209ad158ba3046a1
Nikolai Goncharov <kolchindru@gmail.com>
2025-03-14 14:46:02 +1100

    fix

--- bundle_adjustment_jax.py
+++ bundle_adjustment_jax.py
@@ -2,6 +2,22 @@ import jax.numpy as jnp
 from bundle_adjustment import read_bal_data, visualize_data
 import os
 import urllib
+from jax.scipy.spatial.transform import Rotation as R
+
+
+def project(points, camera_params):
+    rotations = R.from_euler("xyz", camera_params[:, :3]).as_matrix()
+    points_proj = jnp.matmul(rotations, points[..., None]).squeeze(-1)
+    points_proj += camera_params[:, 3:6]
+    points_proj = -points_proj[:, :2] / points_proj[:, 2, jnp.newaxis]
+    f = camera_params[:, 6]
+    k1 = camera_params[:, 7]
+    k2 = camera_params[:, 8]
+    n = jnp.sum(points_proj**2, axis=1)
+    r = 1 + k1 * n + k2 * n**2
+    points_proj *= (r * f)[:, jnp.newaxis]
+    return points_proj
+
 
 if __name__ == "__main__":
     # LOAD DATA
@@ -14,10 +30,4 @@ if __name__ == "__main__":
     camera_params, points_3d, camera_indices, point_indices, points_2d = [
         jnp.array(array) for array in data
     ]
-    print(
-        camera_params.shape,
-        points_3d.shape,
-        camera_indices.shape,
-        point_indices.shape,
-        points_2d.shape,
-    )
+    projected_points = project(points_3d[point_indices], camera_params[camera_indices])
