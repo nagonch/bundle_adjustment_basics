e411b3ddc62af3b2799e3aeb2bb917abc2e8711a
Nikolai Goncharov <kolchindru@gmail.com>
2025-03-17 12:24:21 +1100

    fix

--- jax_sparse.py
+++ jax_sparse.py
@@ -3,8 +3,35 @@ import urllib
 import os
 from bundle_adjustment import read_bal_data
 import jax
+from jax.experimental import sparse
+
 
 jax.config.update("jax_enable_x64", True)
+
+
+def get_jacobian(
+    n_cameras,
+    n_points,
+    camera_indices,
+    point_indices,
+    dr=None,
+    dcamera_params=None,
+    dpoint_indices=None,
+):
+    m = camera_indices.size * 2
+    n = n_cameras * 9 + n_points * 3
+    J = sparse.COO(indices=([], []), values=[], shape=(m, n))
+    if dr is None or dcamera_params is None or dpoint_indices is None:
+        i = jnp.arange(camera_indices.size)
+        for s in range(9):
+            J[2 * i, camera_indices * 9 + s] = 1
+            J[2 * i + 1, camera_indices * 9 + s] = 1
+
+        for s in range(3):
+            J[2 * i, n_cameras * 9 + point_indices * 3 + s] = 1
+            J[2 * i + 1, n_cameras * 9 + point_indices * 3 + s] = 1
+    return J
+
 
 if __name__ == "__main__":
     # LOAD DATA
